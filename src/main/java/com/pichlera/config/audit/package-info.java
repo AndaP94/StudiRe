7e2a3841d4629aec94bec03f94a51a92efcb87a7
Andreas Pichler <andreaspichler@tsn.at>
2019-04-24 15:33:42 +0200

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/com/pichlera/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.pichlera.config.audit;
---
+++ src/main/java/com/pichlera/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.pichlera.repository;
+
+import com.pichlera.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
